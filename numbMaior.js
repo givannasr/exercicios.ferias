6c3850fb5b70211e1d8e3171271af5ed9ff26db6
Giovanna Rocha <126299758+givannasr@users.noreply.github.com>
2023-07-28 10:36:29 -0300

    Update numbMaior.js

--- numbMaior.js
+++ numbMaior.js
@@ -2,16 +2,15 @@ function encontrarMaiorNumero(num1, num2, num3) {
     const numeros = [num1, num2, num3];
     const maior = Math.max(...numeros);
     return maior;
-  }
-  
-  function exibirMaiorNumero() {
+}
+
+function exibirMaiorNumero() {
     const numero1 = parseInt(document.getElementById('num1').value);
     const numero2 = parseInt(document.getElementById('num2').value);
     const numero3 = parseInt(document.getElementById('num3').value);
-  
+
     const maiorNumero = encontrarMaiorNumero(numero1, numero2, numero3);
     document.getElementById('resultado').innerHTML = "O maior número é: " + maiorNumero;
-  }
-  
-  document.getElementById('btnEncontrarMaior').addEventListener('click', exibirMaiorNumero);
-  
+}
+
+document.getElementById('btnEncontrarMaior').addEventListener('click', exibirMaiorNumero);
