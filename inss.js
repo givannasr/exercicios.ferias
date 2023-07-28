be776c199bf0191205b5930ff2e6dee7066c83c7
Giovanna Rocha <126299758+givannasr@users.noreply.github.com>
2023-07-28 10:33:41 -0300

    Update inss.js

--- inss.js
+++ inss.js
@@ -1,37 +1,32 @@
 function calcularDescontoINSS() {
-    let salarioBruto = parseInt(document.getElementById('num1').value)
-    let salarioLiquido = 0;
-    let descontoINSS = 0;
-    let aliquota = 0;
-    if (salarioBruto <= 1320.00) {
-      aliquota = 7.5;
-      descontoINSS = salarioBruto * (aliquota / 100);
-      salarioLiquido = salarioBruto - descontoINSS;
-      document.write("resultados: salário após o desconto: R$"+salarioLiquido+"<br>valor do desconto: R$"+descontoINSS+"<br>e qual o valor da alíquota que ele se enquadrou: "+aliquota);
-    } else if (salarioBruto <= 2571.29) {
-      aliquota = 9.0;
-      descontoINSS = salarioBruto * (aliquota / 100);
-      salarioLiquido = salarioBruto - descontoINSS;
-      document.write("resultados: salário após o desconto: R$"+salarioLiquido+"<br>valor do desconto: R$"+descontoINSS+"<br>e qual o valor da alíquota que ele se enquadrou: "+aliquota);
-    } else if (salarioBruto <= 3856.94) {
-      aliquota = 12.0;
-      descontoINSS = salarioBruto * (aliquota / 100);
-      salarioLiquido = salarioBruto - descontoINSS;
-      document.write("resultados: salário após o desconto: R$"+salarioLiquido+"<br>valor do desconto: R$"+descontoINSS+"<br>e qual o valor da alíquota que ele se enquadrou: "+aliquota);
-    } else if (salarioBruto <= 7507.49) {
-      aliquota = 14.0;
-      descontoINSS = salarioBruto * (aliquota / 100);
-      salarioLiquido = salarioBruto - descontoINSS;
-      document.write("resultados: salário após o desconto: R$"+salarioLiquido+"<br>valor do desconto: R$"+descontoINSS+"<br>e qual o valor da alíquota que ele se enquadrou: "+aliquota);
-    } else {
-      aliquota = 14.0;
-      descontoINSS = 7507.49 * (aliquota / 100);
-      salarioLiquido = salarioBruto - descontoINSS;
-      document.write("resultados: salário após o desconto: R$"+salarioLiquido+"<br>valor do desconto: R$"+descontoINSS+"<br>e qual o valor da alíquota que ele se enquadrou: "+aliquota);
-    }
-    return {
-      salarioLiquido: salarioLiquido.toFixed(2),
-      descontoINSS: descontoINSS.toFixed(2),
-      aliquota: `${aliquota.toFixed(2)}%`
-    };
+  let salarioBruto = parseInt(document.getElementById('num1').value)
+  let salarioLiquido = 0;
+  let descontoINSS = 0;
+  let aliquota = 0;
+  if (salarioBruto <= 1320.00) {
+    aliquota = 7.5;
+    descontoINSS = salarioBruto * (aliquota / 100);
+    salarioLiquido = salarioBruto - descontoINSS;
+    document.write("resultados: salário após o desconto: R$" + salarioLiquido.toFixed(2) + "<br>valor do desconto: R$" + descontoINSS.toFixed(2) + "<br>e qual o valor da alíquota que ele se enquadrou: " + aliquota.toFixed(2));
+  } else if (salarioBruto <= 2571.29) {
+    aliquota = 9.0;
+    descontoINSS = salarioBruto * (aliquota / 100);
+    salarioLiquido = salarioBruto - descontoINSS;
+    document.write("resultados: salário após o desconto: R$" + salarioLiquido.toFixed(2) + "<br>valor do desconto: R$" + descontoINSS.toFixed(2) + "<br>e qual o valor da alíquota que ele se enquadrou: " + aliquota.toFixed(2));
+  } else if (salarioBruto <= 3856.94) {
+    aliquota = 12.0;
+    descontoINSS = salarioBruto * (aliquota / 100);
+    salarioLiquido = salarioBruto - descontoINSS;
+    document.write("resultados: salário após o desconto: R$" + salarioLiquido.toFixed(2) + "<br>valor do desconto: R$" + descontoINSS.toFixed(2) + "<br>e qual o valor da alíquota que ele se enquadrou: " + aliquota.toFixed(2));
+  } else if (salarioBruto <= 7507.49) {
+    aliquota = 14.0;
+    descontoINSS = salarioBruto * (aliquota / 100);
+    salarioLiquido = salarioBruto - descontoINSS;
+    document.write("resultados: salário após o desconto: R$" + salarioLiquido.toFixed(2) + "<br>valor do desconto: R$" + descontoINSS.toFixed(2) + "<br>e qual o valor da alíquota que ele se enquadrou: " + aliquota.toFixed(2));
+  } else {
+    aliquota = 14.0;
+    descontoINSS = 7507.49 * (aliquota / 100);
+    salarioLiquido = salarioBruto - descontoINSS;
+    document.write("resultados: salário após o desconto: R$" + salarioLiquido.toFixed(2) + "<br>valor do desconto: R$" + descontoINSS.toFixed(2) + "<br>e qual o valor da alíquota que ele se enquadrou: " + aliquota.toFixed(2));
   }
+}
